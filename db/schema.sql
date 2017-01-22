2dcb52fac6a8e90bb4d63829a64c94beaba01d78
Neethu Jose <ammu.jose@gmail.com>
2017-01-21 20:39:36 -0500

    node express handlebar app

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db; 
 
-CREATE TABLE burgers
-(
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOT NULL,
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
     devoured TINYINT(1) DEFAULT 0,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
